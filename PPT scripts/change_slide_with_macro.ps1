3ba8d0b160fa9ced9f4587ccfce933bd29fff034
Jordan Soans <jordansoans2002@gmail.com>
2024-08-21 21:24:36 +0530

    removed old code

--- PPT scripts/change_slide_with_macro.ps1
+++
@@ -1,6 +0,0 @@
-Add-type -AssemblyName office
-$application = New-Object -ComObject PowerPoint.Application
-$presentation = $application.Presentations.Open("C:\Users\admin\Desktop\Church\Church-applications\PPT macros\Macro presentation.potm")
-$presentation.Application.Run("Macro presentation.potm!NextSlideInAllPresentations")
-Start-Sleep -s 5
-$presentation.Close()
